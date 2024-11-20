58d266263de1d7b563b715e6f99c69073c5aaf6c
sweshelo <sweshelo@gmail.com>
2024-11-21 00:09:03 +0900

    dark mode無効化

--- src/components/common/small-headline.tsx
+++ src/components/common/small-headline.tsx
@@ -4,8 +4,8 @@ interface SmallHeadlineProps {
 
 export const SmallHeadline = ({ title }: SmallHeadlineProps) => {
   return (
-    <div className="bg-gray-500 p-[3] rounded-lg">
-      <h3 className="text-lg text-center bg-gray-300 rounded">{title}</h3>
+    <div className="p-[3] rounded-lg bg-gray-300 shadow">
+      <h3 className="text-lg text-center bg-gray-200 rounded">{title}</h3>
     </div>
   )
 }
