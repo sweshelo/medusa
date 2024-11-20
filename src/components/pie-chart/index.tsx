bc52f25a1757480ff02e03af9ae5d10c726d09bd
sweshelo <sweshelo@gmail.com>
2024-11-20 20:42:53 +0900

    hydration error 回避

--- src/components/pie-chart/index.tsx
+++ src/components/pie-chart/index.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useEffect, useState } from 'react'
 import { Cell, Pie, PieChart } from 'recharts'
 
 import { Chara } from '@/constants/chara'
@@ -10,23 +11,36 @@ interface RankingPieChartProps {
 }
 
 export const RankingPieChart = ({ ranking }: RankingPieChartProps) => {
-  const counts = ranking.reduce((acc, player) => {
-    const curr = acc.get(player.chara) || 0
-    return acc.set(player.chara, curr + 1)
-  }, new Map<string, number>())
+  const [data, setData] = useState<
+    {
+      id: string
+      name: string
+      color: string
+      count?: number
+    }[]
+  >([])
 
-  const data = Object.entries(Chara)
-    .map(([id, { name, color }]) => {
-      return {
-        id,
-        name,
-        color,
-        count: counts.get(id),
-      }
-    })
-    .filter(r => r.count && r.count > 0)
+  useEffect(() => {
+    const counts = ranking.reduce((acc, player) => {
+      const curr = acc.get(player.chara) || 0
+      return acc.set(player.chara, curr + 1)
+    }, new Map<string, number>())
 
-  return (
+    setData(
+      Object.entries(Chara)
+        .map(([id, { name, color }]) => {
+          return {
+            id,
+            name,
+            color,
+            count: counts.get(id),
+          }
+        })
+        .filter(r => r.count && r.count > 0)
+    )
+  }, [setData])
+
+  return data.length > 0 ? (
     <PieChart width={350} height={210}>
       <Pie
         data={data}
@@ -43,5 +57,5 @@ export const RankingPieChart = ({ ranking }: RankingPieChartProps) => {
         ))}
       </Pie>
     </PieChart>
-  )
+  ) : null
 }
