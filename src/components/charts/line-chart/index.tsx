1b785e6f3a89ac43342274817cb53d8e77fcbb12
sweshelo <sweshelo@gmail.com>
2024-11-20 23:55:49 +0900

    グラフ描画 highchartsに移行

--- src/components/charts/line-chart/index.tsx
+++ src/components/charts/line-chart/index.tsx
@@ -1,16 +1,9 @@
 'use client'
 
 import { format, parse } from 'date-fns'
-import { useCallback, useEffect, useState } from 'react'
-import {
-  CartesianGrid,
-  Line,
-  LineChart,
-  ResponsiveContainer,
-  Tooltip,
-  XAxis,
-  YAxis,
-} from 'recharts'
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+import { useEffect, useState } from 'react'
 
 import { Record as CCJRecord } from '@/types/record'
 
@@ -19,15 +12,14 @@ interface PointsLineChartProps {
 }
 
 export const PointsLineChart = ({ records }: PointsLineChartProps) => {
-  const [avgs, setAvgs] = useState<{ date: string; avg: number }[]>([])
-  const [min, setMin] = useState(50)
-  const [max, setMax] = useState(50)
+  const [chartOptions, setChartOptions] = useState<Highcharts.Options>({})
 
   useEffect(() => {
     // 日毎に分ける
     const grouped = records.reduce<
       Record<string, { total: number; counts: number; records: number[] }>
     >((acc, record) => {
+      console.log(record.datetime.date)
       const date = format(new Date(record.datetime.date), 'yy/MM/dd')
       if (!acc[date]) acc[date] = { total: 0, counts: 0, records: [] }
       acc[date].total += record.diff
@@ -37,43 +29,66 @@ export const PointsLineChart = ({ records }: PointsLineChartProps) => {
     }, {})
 
     // 日毎の平均点を出す
-    const result = Object.entries(grouped).map(([date, { total, counts }]) => ({
+    const entries = Object.entries(grouped).map(([date, { total, counts, records }]) => ({
       date,
       avg: total / counts,
+      max: Math.max(...records),
+      min: Math.min(...records),
     }))
 
-    setAvgs(result)
-    setMin(Math.max(Math.min(...result.map(r => r.avg)) - 10, 50))
-    setMax(Math.max(...result.map(r => r.avg)) + 10)
-  }, [setAvgs, setMin, setMax, records])
+    const dates = entries.map(d => format(parse(d.date, 'yy/MM/dd', new Date()), 'MM/dd'))
+    const averages = entries.map(d => d.avg)
+    const maxes = entries.map(d => d.max)
+    const mines = entries.map(d => d.min)
 
-  const formatter = useCallback((v: number) => {
-    return v.toFixed(2)
-  }, [])
+    setChartOptions({
+      title: {
+        text: undefined,
+      },
+      xAxis: {
+        categories: dates,
+        title: { text: '日付' },
+      },
+      yAxis: {
+        title: { text: null, margin: 0 },
+        labels: { reserveSpace: true },
+      },
+      chart: {
+        spacing: [10, 10, 10, 0],
+      },
+      tooltip: {
+        formatter: function () {
+          return `日付: <b>${this.x}</b><br>貢献度: <b>${this.y?.toFixed(2)}</b>`
+        },
+      },
+      series: [
+        {
+          name: '平均貢献度',
+          data: averages,
+          type: 'spline',
+          color: '#8884d8',
+        },
+        {
+          name: '最高貢献度',
+          data: maxes,
+          type: 'line',
+          color: 'orange',
+          visible: false,
+        },
+        {
+          name: '最低貢献度',
+          data: mines,
+          type: 'line',
+          color: 'blue',
+          visible: false,
+        },
+      ],
+    })
+  }, [records])
 
-  return avgs.length > 0 ? (
-    <div className="bg-white my-2 sm:p-4 p-2 rounded-lg w-full h-[400px] w-[0] max-w-6xl mx-auto">
-      <ResponsiveContainer width={'99%'} height={'100%'}>
-        <LineChart data={avgs} margin={{ top: 20, right: 30, left: 0, bottom: 0 }}>
-          <CartesianGrid strokeDasharray={'3 3'} />
-          <Line type="monotone" dataKey="avg" stroke="#8884d8" activeDot={{ r: 8 }} />
-          <XAxis
-            dataKey={'date'}
-            fontSize={10}
-            height={15}
-            tickFormatter={(d: string) => format(parse(d, 'yy/MM/dd', new Date()), 'MM/dd')}
-          />
-          <YAxis
-            dataKey={'avg'}
-            min={min}
-            fontSize={10}
-            width={25}
-            domain={[min, max]}
-            tickFormatter={v => v.toFixed(0)}
-          />
-          <Tooltip formatter={formatter} />
-        </LineChart>
-      </ResponsiveContainer>
+  return chartOptions ? (
+    <div className="bg-white my-2 sm:p-4 p-2 rounded-lg w-full max-w-6xl mx-auto">
+      <HighchartsReact highcharts={Highcharts} options={chartOptions} />
     </div>
   ) : null
 }
