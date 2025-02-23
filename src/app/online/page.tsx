b6c7414bf2a8952b93689ca73dd59a8295710cc4
sweshelo <sweshelo@gmail.com>
2025-02-23 21:18:33 +0900

    refactor: 💡 300秒キャッシュ

--- src/app/online/page.tsx
+++ src/app/online/page.tsx
@@ -1,6 +1,8 @@
 import { OnlinePlayers } from '@/features/online'
 import { fetchOnlinePlayers } from '@/service/supabase/online'
 
+export const revalidate = 300
+
 export default async function Page() {
   const players = await fetchOnlinePlayers()
   return <OnlinePlayers players={players} />
