d0f750e7c7ac4cfa15ae919fd29ab846edd881c9
sweshelo <sweshelo@gmail.com>
2025-02-23 21:02:46 +0900

    fix: 🐛 時差が発生するのを修正

--- src/features/online/index.tsx
+++ src/features/online/index.tsx
@@ -1,7 +1,9 @@
+import { TZDate } from '@date-fns/tz'
 import { differenceInMinutes } from 'date-fns'
 
 import { Headline } from '@/components/common/headline'
 import { PlayerCard } from '@/components/player/card'
+import { Revalidater } from '@/components/revalidater'
 
 interface Props {
   players: {
@@ -15,15 +17,15 @@ export const OnlinePlayers = ({ players }: Props) => {
   return (
     <>
       <Headline title="オンラインのプレイヤー" />
+      <Revalidater />
       <div className="mt-4 mb-4 space-y-2">
         {players.map((player, index) => (
           <PlayerCard player={{ name: player.player_name }} chara={player.chara} key={index}>
             <div className="flex">
               <div className="text-sm text-gray-600 ml-1">
-                {player.recorded_at 
-                  ? `${differenceInMinutes(new Date(), new Date(player.recorded_at))}分前`
-                  : '時刻不明'
-                }
+                {player.recorded_at
+                  ? `${differenceInMinutes(new Date(), new TZDate(player.recorded_at, 'Asia/Tokyo'))}分前`
+                  : '時刻不明'}
               </div>
             </div>
           </PlayerCard>
