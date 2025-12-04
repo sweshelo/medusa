38f79d7e8ba38e6813ef2c5136c6dbb7771fbed9
sweshelo <sweshelo@gmail.com>
2025-12-04 23:43:54 +0900

    不用意にServiceRoleKeyを用いない

--- src/service/supabase/server.ts
+++ src/service/supabase/server.ts
@@ -7,8 +7,8 @@ export async function createClient() {
   const cookieStore = await cookies()
 
   return createServerClient<Database>(
-    process.env.SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {
